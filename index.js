1e198a2b02b051804bc6bb62fa9f2b0b1e618fb2
Oleg Konan <olegkonanff@gmail.com>
2021-01-17 20:24:35 +0300

    fix font

--- index.js
+++ index.js
@@ -4,14 +4,20 @@ const mainElement = document.querySelector('.wrapper');
 
 mainElement.innerHTML = plot
   .map(element => {
-    return `
-    <div class="replica">
-      <p class="id">${element.id}</p>
-      <p class="act">${element.act}</p>
-      <p class="scene">${element.scene}</p>
-      <p class="character">${element.character}</p>
-      <p class="text">${element.text}</p>
-    </div>
-    `
+    if (element.isDescription === true) {
+      return `
+        <div class="replica">
+          <p class="text description">${element.text}</p>
+        </div>`
+    } else {
+      return `
+        <div class="replica">
+          <p class="id">${element.id}</p>
+          <p class="act">${element.act}</p>
+          <p class="scene">${element.scene}</p>
+          <p class="character">${element.character}</p>
+          <p class="text">${element.text}</p>
+        </div>`
+    }
   })
   .join('');
