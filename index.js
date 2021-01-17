ec1c6d1071243af7b0aa2967f2d0f8769395a231
Oleg Konan <olegkonanff@gmail.com>
2021-01-17 22:46:26 +0300

    feat: chooseCharacter

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 import { plot } from './data/plot.js';
+import { characters } from './data/characters.js';
 
 const mainElement = document.querySelector('.wrapper');
+const characterSelect = document.querySelector('#character');
 
-mainElement.innerHTML = plot
+characters.forEach(element => {
+  const option = new Option(`${element.name}`, `${element.id}`);
+  characterSelect.appendChild(option);
+});
+
+function selectCharacter(e) {
+  const currChar = e.target.selectedOptions[0].textContent;
+  renderPlot(currChar);
+  localStorage.setItem('currentCharacterName', currChar);
+  localStorage.setItem('currentCharacterId', e.target.value);
+}
+
+function renderPlot(character = 'all') {
+  mainElement.innerHTML = plot
   .map(element => {
-    if (element.isDescription === true) {
+    if (element.isDescription) {
       return `
         <div class="replica">
           <p class="text description">${element.text}</p>
+        </div>`
+    } else if (character === element.character) {
+      return `
+        <div class="replica">
+          <p class="id">${element.id}</p>
+          <p class="act">${element.act}</p>
+          <p class="scene">${element.scene}</p>
+          <p class="character marked">${element.character}</p>
+          <p class="text marked">${element.text}</p>
         </div>`
     } else {
       return `
@@ -21,10 +45,18 @@ mainElement.innerHTML = plot
     }
   })
   .join('');
+}
 
 function savePosition() {
   localStorage.setItem('offsetY', window.scrollY);
 }
 
-window.addEventListener('load', () => window.scrollTo(0, localStorage.getItem('offsetY') || 0));
+window.addEventListener('load', () => {
+  renderPlot(localStorage.getItem('currentCharacterName') || null);
+  characterSelect.value = localStorage.getItem('currentCharacterId') || 0;
+});
+window.addEventListener('load', () => {
+  window.scrollTo(0, localStorage.getItem('offsetY') || 0);
+});
 window.addEventListener('scroll', savePosition);
+characterSelect.addEventListener('change', selectCharacter);
